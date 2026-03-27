06d53d5c734affbc951b33aa39280f71cf269f23
Tomas Ludwig <tomasludwig@TomasMBPro.local>
2026-03-27 19:31:40 +0100

    add color tokens and Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.css'
+
+export function Button({ variant = 'primary', children, onClick }) {
+  return (
+    <button className={`btn btn--${variant}`} onClick={onClick}>
+      {children}
+    </button>
+  )
+}
